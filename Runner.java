b6dc776b817a71a71f8ac24f7160dd3b67d471ff
pranav <prnvkatkar88@gmail.com>
2021-10-21 23:31:36 +0530

    Added:Ability to create stack

--- Runner.java
+++ Runner.java
@@ -3,6 +3,12 @@ public class Runner {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to stack queue");
+		StackQueue sQ = new StackQueue();
+		sQ.push(70);
+		sQ.push(30);
+		sQ.push(56);
+
+		sQ.display();
 
 	}
 
