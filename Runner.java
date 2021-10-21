aa055e81386c35ce45f45c12084445021d802447
pranav <prnvkatkar88@gmail.com>
2021-10-21 23:51:58 +0530

    Added:functionality to peek and pop data

--- Runner.java
+++ Runner.java
@@ -7,7 +7,13 @@ public class Runner {
 		sQ.push(70);
 		sQ.push(30);
 		sQ.push(56);
+		sQ.peek();
+		System.out.println("before popping");
+		sQ.display();
 
+		sQ.popTillEnd();
+		System.out.println();
+		System.out.println("after popping");
 		sQ.display();
 
 	}
--- StackQueue.java
+++ StackQueue.java
@@ -19,12 +19,26 @@ public class StackQueue {
 		return newnode;
 	}
 
+	public int peek() {
+		System.out.println("\nTop element is " + this.top.data);
+		return top.data;
+	}
+
+	public void popTillEnd() {
+		Node temp = top;
+		while (temp != null) {
+			Node tempNode = this.head;
+			this.head = tempNode.next;
+			temp = temp.next;
+		}
+	}
+
 	public void display() {
 		if (head == null) {
 			System.out.println("Linked list is Empty");
 			return;
 		} else {
-			System.out.print("\n The stack elements are ");
+			System.out.print("The stack elements are ");
 			Node tempNode = head;
 			while (tempNode != null) {
 				if (tempNode.next != null) {
@@ -39,4 +53,3 @@ public class StackQueue {
 
 	}
 }
-	
