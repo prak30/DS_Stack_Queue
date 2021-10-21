371829f8f195a4ee32a09b37acf44589a677246e
pranav <prnvkatkar88@gmail.com>
2021-10-22 00:03:38 +0530

    Added:created queue and added values

--- Runner.java
+++ Runner.java
@@ -4,16 +4,10 @@ public class Runner {
 	public static void main(String[] args) {
 		System.out.println("welcome to stack queue");
 		StackQueue sQ = new StackQueue();
-		sQ.push(70);
-		sQ.push(30);
-		sQ.push(56);
-		sQ.peek();
-		System.out.println("before popping");
-		sQ.display();
+		sQ.queue(56);
+		sQ.queue(30);
+		sQ.queue(70);
 
-		sQ.popTillEnd();
-		System.out.println();
-		System.out.println("after popping");
 		sQ.display();
 
 	}
